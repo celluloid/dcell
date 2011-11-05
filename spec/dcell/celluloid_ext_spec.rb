e7d9c75dad881a000bc6c04d7683085a22dbc8ad
Tony Arcieri <tony.arcieri@gmail.com>
2011-11-05 16:22:40 -0700

    More extensive specs for Celluloid extensions

--- spec/dcell/celluloid_ext_spec.rb
+++ spec/dcell/celluloid_ext_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 describe Celluloid, "extensions" do
-  class WillKane
-    include Celluloid
+  before do
+    class WillKane
+      include Celluloid
+    end
+    @marshal = WillKane.new
   end
 
-  it "marshals actors" do
-    marshal = WillKane.new
-    Marshal.dump(marshal).should be_a(String)
+  it "marshals Celluloid::ActorProxy objects" do
+    string = Marshal.dump(@marshal)
+    Marshal.load(string).should be_a(DCell::ActorProxy)
+  end
+
+  it "marshals Celluloid::Mailbox objects" do
+    @marshal.mailbox.should be_a(Celluloid::Mailbox)
+    string = Marshal.dump(@marshal.mailbox)
+    Marshal.load(string).should be_a(DCell::MailboxProxy)
   end
 end
