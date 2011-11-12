c50b9a3ac85d8f1aa2f5cc6b4843c379ab5b7786
Tony Arcieri <tony.arcieri@gmail.com>
2011-11-11 22:09:09 -0800

    Refactor DCell::ZookeeperAdapter

--- lib/dcell/registries/zookeeper_adapter.rb
+++ lib/dcell/registries/zookeeper_adapter.rb
@@ -10,14 +10,18 @@ module DCell
     # servers: a list of Zookeeper servers to connect to. Each server in the
     #          list has a host/port configuration
     def initialize(options)
-      @env = options[:env] || options['env'] || 'production'
+      # Stringify keys :/
+      options = options.inject({}) { |h,(k,v)| h[k.to_s] = v; h }
+
+      @env = options['env'] || 'production'
+      @base_path = "#{PREFIX}/#{@env}"
 
       # Let them specify a single server instead of many
-      server = options[:server] || options['server']
+      server = options['server']
       if server
         servers = [server]
       else
-        servers = options[:servers] || options['servers']
+        servers = options['servers']
         raise "no Zookeeper servers given" unless servers
       end
 
@@ -31,63 +35,68 @@ module DCell
       end
 
       @zk = ZK.new(*servers)
-      @zk.mkdir_p "#{base_path}/nodes"
-      @zk.mkdir_p "#{base_path}/globals"
+      @node_registry = NodeRegistry.new(@zk, @base_path)
+      @global_registry = GlobalRegistry.new(@zk, @base_path)
     end
 
-    #
-    # Node registry
-    #
+    class NodeRegistry
+      def initialize(zk, base_path)
+        @zk, @base_path = zk, "#{base_path}/nodes"
+        @zk.mkdir_p @base_path
+      end
 
-    # Get the address of a particular node
-    def get_node(node_id)
-      result, _ = @zk.get("#{base_path}/nodes/#{node_id}")
-      result
-    rescue ZK::Exceptions::NoNode
+      def get(node_id)
+        result, _ = @zk.get("#{@base_path}/#{node_id}")
+        result
+      rescue ZK::Exceptions::NoNode
+      end
+
+      def set(node_id, addr)
+        path = "#{@base_path}/#{node_id}"
+        @zk.set path, addr
+      rescue ZK::Exceptions::NoNode
+        @zk.create path, addr
+      end
+
+      def nodes
+        @zk.children @base_path
+      end
     end
 
-    # Set the address of a particular node
-    def set_node(node_id, addr)
-      path = "#{base_path}/nodes/#{node_id}"
-      @zk.set path, addr
-    rescue ZK::Exceptions::NoNode
-      @zk.create path, addr
+    def get_node(node_id);       @node_registry.get(node_id) end
+    def set_node(node_id, addr); @node_registry.set(node_id, addr) end
+    def nodes;                   @node_registry.nodes end
+
+    class GlobalRegistry
+      def initialize(zk, base_path)
+        @zk, @base_path = zk, "#{base_path}/globals"
+        @zk.mkdir_p @base_path
+      end
+
+      def get(key)
+        value, _ = @zk.get "#{@base_path}/#{key}"
+        Marshal.load value
+      rescue ZK::Exceptions::NoNode
+      end
+
+      # Set a global value
+      def set(key, value)
+        path = "#{@base_path}/#{key}"
+        string = Marshal.dump value
+
+        @zk.set path, string
+      rescue ZK::Exceptions::NoNode
+        @zk.create path, string
+      end
+
+      # The keys to all globals in the system
+      def global_keys
+        @zk.children(@base_path).map(&:to_sym)
+      end
     end
 
-    # Find all of the nodes on the system
-    def nodes
-      @zk.children "#{base_path}/nodes"
-    end
-
-    #
-    # Globals
-    #
-
-    # Get a global value
-    def get_global(key)
-      value, _ = @zk.get "#{base_path}/globals/#{key}"
-      Marshal.load value
-    rescue ZK::Exceptions::NoNode
-    end
-
-    # Set a global value
-    def set_global(key, value)
-      path = "#{base_path}/globals/#{key}"
-      string = Marshal.dump value
-
-      @zk.set path, string
-    rescue ZK::Exceptions::NoNode
-      @zk.create path, string
-    end
-
-    # The keys to all globals in the system
-    def global_keys
-      @zk.children("#{base_path}/globals").map(&:to_sym)
-    end
-
-    # Base path for all entries
-    def base_path
-      "#{PREFIX}/#{@env}"
-    end
+    def get_global(key);        @global_registry.get(key) end
+    def set_global(key, value); @global_registry.set(key, value) end
+    def global_keys;            @global_registry.global_keys end
   end
 end
