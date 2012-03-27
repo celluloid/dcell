88be581af9f33ec920ec48266f6da3a2026fdcb8
Tony Arcieri <tony.arcieri@gmail.com>
2012-03-26 23:49:17 -0700

    Initial DCell::Explorer spike with Reel

---
+++ lib/dcell/explorer.rb
@@ -0,0 +1,40 @@
+require 'reel'
+require 'pathname'
+
+module DCell
+  class Explorer
+    ASSET_ROOT = Pathname.new File.expand_path("../../../explorer", __FILE__)
+
+    def initialize(host = "127.0.0.1", port = 7778)
+      @server = Reel::Server.new(host, port, &method(:handle_connection))
+    end
+
+    # TODO: real static file service for Reel :/
+    def handle_connection(connection)
+      request = connection.request
+      return unless request
+
+      if request.url == "/"
+        path = "index.html"
+      else
+        path = request.url[%r{^/([a-z0-9\.\-_]+(/[a-z0-9\.\-_]+)*)$}, 1]
+      end
+
+      unless path or path[".."]
+        Celluloid::Logger.info "404 Not Found: #{request.path}"
+        connection.respond :not_found, "Not found"
+        return
+      end
+
+      asset_path = ASSET_ROOT.join(path)
+      if asset_path.exist?
+        file = File.read(asset_path.to_s, :mode => 'rb')
+        connection.respond :ok, file
+        Celluloid::Logger.info "200 OK: #{request.url}"
+      else
+        connection.respond :not_found, "Not found"
+        Celluloid::Logger.info "404 Not Found: #{request.url}"
+      end
+    end
+  end
+end
