083be83781c5f59336f0bff034a74dbdf2c5f691
Tony Arcieri <tony.arcieri@gmail.com>
2012-04-14 10:59:09 -0700

    Workaround for buggy exit handling in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,12 +5,19 @@ Bundler.setup
 require 'dcell'
 Dir['./spec/support/*.rb'].map { |f| require f }
 
-DCell.setup :directory => { :id => 'test_node', :addr => "tcp://127.0.0.1:#{TestNode::PORT}" }
-DCell.run!
+RSpec.configure do |config|
+  config.before(:suite) do
+    DCell.setup :directory => { :id => 'test_node', :addr => "tcp://127.0.0.1:#{TestNode::PORT}" }
+    @supervisor = DCell.run!
 
-TestNode.start
-TestNode.wait_until_ready
+    TestNode.start
+    TestNode.wait_until_ready
+  end
 
-at_exit do
-  TestNode.stop
+  config.after(:suite) do
+    TestNode.stop
+  end
 end
+
+# FIXME: this is hax to bypass the other at_exit handlers
+at_exit { exit! $!.status }
