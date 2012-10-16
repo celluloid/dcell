080a0564a9a4ce3d401ca0abd50353ceee97ad79
Tony Arcieri <tony.arcieri@gmail.com>
2012-10-16 01:08:34 -0700

    Simpler node init in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,8 @@ Dir['./spec/support/*.rb'].map { |f| require f }
 
 RSpec.configure do |config|
   config.before(:suite) do
-    DCell.setup :directory => { :id => 'test_node', :addr => "tcp://127.0.0.1:#{TestNode::PORT}" }
-    @supervisor = DCell.run!
+    DCell.setup
+    DCell.run!
 
     TestNode.start
     TestNode.wait_until_ready
