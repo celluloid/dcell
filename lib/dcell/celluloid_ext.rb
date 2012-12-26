b22f3a0fdc4c5866d24a063d0ee16027d351fdf6
Tony Arcieri <tony.arcieri@gmail.com>
2012-12-25 20:40:05 -0800

    Fix respond_to? arity

--- lib/dcell/celluloid_ext.rb
+++ lib/dcell/celluloid_ext.rb
@@ -14,10 +14,10 @@ module Celluloid
     # unfortunately we have to monkeypatch in _dump support as the proxy
     # itself normally jacks respond_to? and proxies to the actor
     alias_method :__respond_to?, :respond_to?
-    def respond_to?(meth)
+    def respond_to?(meth, check_private = false)
       return false if meth == :marshal_dump
       return true  if meth == :_dump
-      __respond_to? meth
+      __respond_to?(meth, check_private)
     end
 
     # Dump an actor proxy via its mailbox
