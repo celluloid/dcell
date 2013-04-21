b9b82024b0cbd1ea4773650c510242cfd7224cee
Tim Carey-Smith <tim@spork.in>
2013-04-21 17:25:58 +1200

    Disable ZK specs on CI due to no zookeeper server

--- spec/dcell/registries/zk_adapter_spec.rb
+++ spec/dcell/registries/zk_adapter_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'dcell/registries/zk_adapter'
 
-describe DCell::Registry::ZkAdapter do
+describe DCell::Registry::ZkAdapter, :pending => ENV["CI"] && "no zookeeper" do
   subject { DCell::Registry::ZkAdapter.new :server => 'localhost', :env => 'test' }
   it_behaves_like "a DCell registry"
 end
