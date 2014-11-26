d6a5ef9ce9557b81d05b5f2f9375bc11674780b5
Dmytro Milinevskyy <milinevskyy@gmail.com>
2014-11-26 01:19:10 +0100

    spec: cover explorer

--- spec/dcell/explorer_spec.rb
+++ spec/dcell/explorer_spec.rb
@@ -5,12 +5,32 @@ describe DCell::Explorer do
   EXPLORER_PORT = 7778
   EXPLORER_BASE = "http://#{EXPLORER_HOST}:#{EXPLORER_PORT}"
 
-  before do
+  before :all do
     @explorer = DCell::Explorer.new(EXPLORER_HOST, EXPLORER_PORT)
   end
 
   it "reports the current node's status" do
     response = Net::HTTP.get URI(EXPLORER_BASE)
     response[%r{<a href="/nodes/(.*?)">}, 1].should == DCell.id
+  end
+
+  it "reports a given current node status" do
+    response = Net::HTTP.get URI(EXPLORER_BASE + "/nodes/#{DCell.id}")
+    response[%r{<a href="/nodes/(.*?)">}, 1].should == DCell.id
+  end
+
+  it "respond with 404 for non-existing node or paths" do
+    response = Net::HTTP.get URI(EXPLORER_BASE + "/nodes/node")
+    response.should == "Not found"
+    response = Net::HTTP.get URI(EXPLORER_BASE + "/my.html")
+    response.should == "Not found"
+    response = Net::HTTP.get URI(EXPLORER_BASE + "/..")
+    response.should == "Not found"
+  end
+
+  it "correctly return web resources" do
+    response = Net::HTTP.get URI(EXPLORER_BASE + "/css/explorer.css")
+    favicon = File.expand_path("../../../explorer/css/explorer.css", __FILE__)
+    response.should == File.open(favicon, 'r').read
   end
 end
