b1761df033487dad0c71a519e19db72f8f46b71b
Dmytro Milinevskyy <milinevskyy@gmail.com>
2014-12-17 01:52:42 +0100

    examples:scratcy: expose new dcell capabilities

--- examples/scratchy.rb
+++ examples/scratchy.rb
@@ -5,17 +5,21 @@ DCell.start
 itchy_node = DCell::Node["itchy"]
 itchy = itchy_node[:itchy]
 
+puts "All itchy actors: #{itchy_node.all}"
 puts "Fighting itchy! (check itchy's output)"
 
 300.times do
   begin
+    itchy.async.fight
+    future = itchy.future.fight
     puts itchy_node[:itchy].fight
     puts itchy.fight
+    puts future.value
   rescue Celluloid::DeadActorError
     puts "Itchy dying?"
     itchy = itchy_node[:itchy]
   rescue => e
-    puts e
+    puts "Unknow error #{e}"
     break
   end
   sleep 1
