45d2f486e3ca854c94e2e5781214bd291b968b83
Dmytro Milinevskyy <milinevskyy@gmail.com>
2015-02-28 16:18:48 +0100

    examples: explorer launcher

---
+++ examples/explorer.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require 'dcell/explorer'
+require_relative 'registry'
+
+explorer_host = 'localhost'
+explorer_port = 7778
+
+DCell.start :registry => registry
+DCell::Explorer.new explorer_host, explorer_port
+
+puts "Visit explorer page at http://#{explorer_host}:#{explorer_port}"
+sleep
