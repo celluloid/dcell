a352b907e03228a029d5b052954cba114537819c
Dmytro Milinevskyy <milinevskyy@gmail.com>
2015-05-03 10:59:58 +0200

    spec: close socket on the exit from the test case

--- spec/dcell/socket_spec.rb
+++ spec/dcell/socket_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe DCell::Socket do
 
   it "raises exception if address is already in use" do
     addr = 'tcp://127.0.0.1:1123'
-    server = DCell::Socket::server addr, ''
+    server, raddr = DCell::Socket::server addr, ''
     expect {DCell::Socket::server addr, ''}.to raise_error(IOError)
+    server.close
   end
 end
