ebc24cc6137daac5d192d5a4360a0b3429d53477
Dmytro Milinevskyy <milinevskyy@gmail.com>
2015-05-03 13:45:20 +0000

    node: open relay pipe early to avoid race conditiions with intermixed async/future/sync DRPC calls

--- lib/dcell/node.rb
+++ lib/dcell/node.rb
@@ -61,6 +61,7 @@ module DCell
       request = Message::Find.new(Thread.mailbox, name)
       methods = send_request request
       return nil if methods.is_a? NilClass
+      rsocket # open relay pipe to avoid race conditions
       actor = DCell::ActorProxy.create.new self, name, methods
       add_actor actor
     end
